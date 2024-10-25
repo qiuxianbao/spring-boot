397f46ab6ad92ef89725f3a8a3ea80568f9cc9e7
邱献宝 <qiuxianbao@aipark.com>
2024-10-25 15:31:01 +0800

    feat(gradle): rename

---
+++ learn-lifecycle/src/main/java/cn/thinkinjava/spring/LearnLifecycleApplication.java
@@ -0,0 +1,19 @@
+package cn.thinkinjava.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 生命周期
+ *
+ * @author qiuxianbao
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class LearnLifecycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnLifecycleApplication.class, args);
+    }
+}
